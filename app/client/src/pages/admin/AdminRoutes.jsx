fbb4c051f15eb5410a65f0d89f7051e4093682a1
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-09-15 16:42:27 +0530

    AdminLandingPage added

--- app/client/src/pages/admin/AdminRoutes.jsx
+++ app/client/src/pages/admin/AdminRoutes.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import AdminLandingPage from './AdminLandingPage'
+
 
 const AdminRoutes = () => {
   return (
-    <div>
-      AdminRoutes
+    <div className='pt-20'>
+<Routes>
+      <Route path="/" element={<AdminLandingPage />} />
+      {/* You can add more admin-specific routes here later */}
+      {/* <Route path="/dashboard" element={<AdminDashboard />} /> */}
+      {/* <Route path="/reports" element={<AdminReports />} /> */}
+    </Routes>
     </div>
   )
 }
